b19b6f8c153d541e6b6e9bda1eae5338ea259abe
mzmpy <2316687327@qq.com>
2022-07-18 21:59:32 +0800

    Clock component add button controlling

--- src/clock.js
+++ src/clock.js
@@ -4,7 +4,8 @@ export default class Clock extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      date: new Date()
+      date: new Date(),
+      status: true
     }
   }
 
@@ -22,10 +23,25 @@ export default class Clock extends React.Component {
     })
   }
 
+  toggle = () => {
+    if(this.state.status) {
+      clearInterval(this.timer)
+    } else {
+      this.timer = setInterval(() => this.tick(), 1000)
+    }
+
+    this.setState((state) => {
+      return {
+        status: !state.status
+      }
+    })
+  }
+
   render() {
       return <>
         <h1>Hello, { this.props.name }!</h1>
         <h2>It is { this.state.date.toLocaleTimeString() } now.</h2>
+        <button onClick={ this.toggle }>{ this.state.status ? 'stop' : 'react' }</button>
       </>
   }
 }
