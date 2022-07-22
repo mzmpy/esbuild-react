7c3c839df1e2f12e25b7b47efda24243128ff885
mzmpy <2316687327@qq.com>
2022-07-22 08:39:01 +0800

    replace 'esbuild.serve' with 'esbuild.build'

--- build/build.js
+++ build/build.js
@@ -1,6 +1,13 @@
 const esbuild = require('esbuild')
 
-esbuild.buildSync({
+esbuild.serve({
+  port: 3888,
+  host: '127.0.0.1',
+  servedir: '/dist',
+  onRequest: (args) => {
+    console.log(`${args.method}-User ${args.remoteAddress} accesses path ${args.path} with server responsing status code ${args.status} in ${args.timeInMS} ms.`)
+  }
+}, {
   entryPoints: ['./src/index.js'],
   bundle: true,
   minify: true,
@@ -8,4 +15,6 @@ esbuild.buildSync({
     '.js': 'jsx'
   },
   outfile: './dist/index.js'
+}).then((service) => {
+  console.log(`Esbuild serve at http://${service.host}:${service.port}.`)
 })
